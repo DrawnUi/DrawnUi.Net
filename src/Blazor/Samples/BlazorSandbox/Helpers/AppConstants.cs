f34d339af7719318e90170f978881b7efde768eb
AppoMobi <taublast@gmail.com>
2026-05-22 14:19:16 +0200

    use latest nuget

--- src/Blazor/Samples/BlazorSandbox/Helpers/AppConstants.cs
+++ src/Blazor/Samples/BlazorSandbox/Helpers/AppConstants.cs
@@ -2,7 +2,7 @@ namespace BlazorSandbox.Helpers;
 
 public static class AppConstants
 {
-    public const string Version = "1.0.0.2";
+    public const string Version = "1.0.0.4";
 
     public const string LastSeenVersionKey = "app.version";
 }
